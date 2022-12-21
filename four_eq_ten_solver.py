dc2efd3e385bb14876fe979f54a92bafe75079fd
I-Kerosin-I <max.andr2005@yandex.ru>
2022-12-21 23:19:55 +0300

    Versinon 1.1 with permutations of digits

--- four_eq_ten_solver.py
+++ four_eq_ten_solver.py
@@ -1,12 +1,15 @@
-from itertools import product
+from itertools import product, permutations
 
 
-def solver(n1, n2, n3, n4):
-    res = [
-        f'{br[0]} {n1} {i[0]} {br[1]} {n2} {br[2]} {i[1]} {br[3]} {n3} {br[4]} {i[2]} {n4} {br[5]}' \
-        for i in product('+-*/', repeat=3) for br in
-        ('      ', '( )   ', '(   ) ', ' (  ) ', ' (   )', '   ( )')]
-    print([i.replace(' ', '') for i in res if eval(i) == 10])
+def solver(d1, d2, d3, d4):
+    for n in permutations(f'{d1}{d2}{d3}{d4}'):
+        res = [
+            f'{br[0]} {n[0]} {i[0]} {br[1]} {n[1]} {br[2]} {i[1]} {br[3]} {n[2]} {br[4]} {i[2]} {n[3]} {br[5]}' \
+            for i in product('+-*/', repeat=3) for br in
+            ('      ', '( )   ', '(   ) ', ' (  ) ', ' (   )', '   ( )')]
+        try:
+            print([i.replace(' ', '') for i in res if eval(i) == 10])
+        except ZeroDivisionError:
+            print('ZeroDivision')
 
-
-solver(1, 9, 5, 6)
+solver(4,1,2,8)
