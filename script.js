7a46c3b5770bbe61a4ea76785ce3e6c25658d424
I-Kerosin-I <max.andr2005@yandex.ru>
2022-12-26 23:09:01 +0300

    Moves queue added

--- script.js
+++ script.js
@@ -14,28 +14,29 @@ const div = (a, b) => ((a / b) | 0)
 
 const size = 10
 let snake = [{x: 0, y: 0}]
-let dx = 1, dy = 0, dx_ = 1, dy_ = 0
 let apple = {x: 100, y: 100}
+let queue = [{dx: 1, dy: 0}]
+let dx = 1, dy = 0, dx_ = 1, dy_ = 0
 
 document.addEventListener('keydown' ,ev => {if (ev.keyCode === 107) {apple.x = snake[0].x +dx*size; apple.y = snake[0].y+dy*size;}})
 document.addEventListener('keydown', (ev) =>{
     switch (ev.keyCode) {
         case 38:
-            dx = 0
-            dy = dy_ !== 1? -1 : 1;
-            break
+            if ((queue.length > 0 ? queue[0].dy : dy) !== 1) {
+                queue.unshift({dx: 0, dy: -1})
+            }break
         case 40:
-            dx = 0
-            dy = dy_ !== -1? 1 : -1;
-            break
+            if ((queue.length > 0 ? queue[0].dy : dy) !== -1) {
+                queue.unshift({dx: 0, dy: 1})
+            }break
         case 37:
-            dx = dx_ !== 1? -1 : 1;
-            dy = 0
-            break
+            if ((queue.length > 0 ? queue[0].dx : dx) !== 1) {
+                queue.unshift({dx: -1, dy: 0})
+            }break
         case 39:
-            dx = dx_ !== -1? 1 : -1;
-            dy = 0
-            break
+            if ((queue.length > 0 ? queue[0].dx : dx) !== -1) {
+                queue.unshift({dx: 1, dy: 0})
+            }break
     }
 })
 setInterval(() => {
@@ -44,7 +45,10 @@ setInterval(() => {
     ctx.fillStyle = 'green'
     ctx.fillRect(apple.x, apple.y, size, size)
     ctx.fillStyle = 'red'
-    snake.unshift({x: mod(snake[0].x+dx*size, canv.width - canv.width % size), y: mod(snake[0].y+dy*size, canv.height - canv.height % size)})
+    if (queue.length > 0){({dx, dy} = queue.pop())}
+    snake.unshift({
+        x: mod(snake[0].x+dx*size, canv.width - canv.width % size),
+        y: mod(snake[0].y+dy*size, canv.height - canv.height % size)})
     dx_ = dx; dy_ = dy;
     if (snake[0].x === apple.x && snake[0].y === apple.y) {
         apple.x = Math.floor(Math.random() * div(canv.width, size)) * size
@@ -57,4 +61,4 @@ setInterval(() => {
     for (let j = 0; j < snake.length; j++) {
         ctx.fillRect(snake[j].x, snake[j].y, size, size)
     }
-}, 50)
+}, 30)
